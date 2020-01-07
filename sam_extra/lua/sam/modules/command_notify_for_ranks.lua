82d0fb1fc70aa1d232bbe83ae55b86814f41b889
ahmed wagih <srlion32@gmail.com>
2020-01-07 04:00:11 +0200

    made notifying detour only serverside

--- sam_extra/lua/sam/modules/command_notify_for_ranks.lua
+++ sam_extra/lua/sam/modules/command_notify_for_ranks.lua
@@ -6,21 +6,23 @@ if SAM_LOADED then return end
 
 sam.permissions.add("command_notify", nil, "superadmin")
 
-local get_players = function()
-    local players = {}
-    for _, v in ipairs(player.GetAll()) do
-        if v:HasPermission("command_notify") then
-            table.insert(players, v)
+if SERVER then
+    local get_players = function()
+        local players = {}
+        for _, v in ipairs(player.GetAll()) do
+            if v:HasPermission("command_notify") then
+                table.insert(players, v)
+            end
         end
+        return players
     end
-    return players
-end
 
-local old_send = sam.player.send_message
-function sam.player.send_message(ply, msg, tbl)
-    if ply == nil and debug.traceback():find("lua/sam/command/", 1, true) then
-        old_send(get_players(), msg, tbl)
-    else
-        old_send(ply, msg, tbl)
+    local old_send = sam.player.send_message
+    function sam.player.send_message(ply, msg, tbl)
+        if ply == nil and debug.traceback():find("lua/sam/command/", 1, true) then
+            old_send(get_players(), msg, tbl)
+        else
+            old_send(ply, msg, tbl)
+        end
     end
 end
