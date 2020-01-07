48ed4ff17fb4daa8452ec1f1b9652ab37ec084be
ahmed wagih <srlion32@gmail.com>
2020-01-07 04:34:30 +0200

    Added hidden admin name

---
+++ sam_extra/lua/sam/modules/command_hide_admin_name.lua
@@ -0,0 +1,37 @@
+--
+-- This hides admins names when running commands
+-- eg. *Someone slapped everyone.
+-- You can NOT use this with 'command_notify_for_ranks.lua'
+--
+if SAM_LOADED then return end
+
+sam.permissions.add("see_hidden_admin_name", nil, "admin")
+
+if SERVER then
+    sam.player.old_send_message = sam.player.old_send_message or sam.player.send_message
+    function sam.player.send_message(ply, msg, tbl)
+        if ply or not debug.traceback():find("lua/sam/command/", 1, true) or not tbl or not msg then
+            return sam.player.old_send_message(ply, msg, tbl)
+        end
+
+		msg = sam.language.get(msg) or msg
+
+        local admins, players = {}, {}
+        for _, v in ipairs(player.GetAll()) do
+            table.insert(v:HasPermission("see_hidden_admin_name") and admins or players, v)
+        end
+
+        sam.player.old_send_message(admins, msg, tbl)
+
+        msg = msg:gsub("%{A%}", function()
+            local admin = tbl["A"]
+            if admin == sam.console then
+                return "{A}"
+            end
+            tbl["A"] = "Someone"
+            return "*{A}"
+        end, 1)
+
+        sam.player.old_send_message(players, msg, tbl)
+    end
+end
--- sam_extra/lua/sam/modules/command_notify_for_ranks.lua
+++ sam_extra/lua/sam/modules/command_notify_for_ranks.lua
@@ -1,10 +1,11 @@
 --
 -- Make command notifying only for ranks you select.
 -- permission is command_notify. (by default admin+ has it)
+-- You can NOT use this with 'command_hide_admin_name.lua'
 --
 if SAM_LOADED then return end
 
-sam.permissions.add("command_notify", nil, "superadmin")
+sam.permissions.add("command_notify", nil, "admin")
 
 if SERVER then
     local get_players = function()
@@ -17,12 +18,12 @@ if SERVER then
         return players
     end
 
-    local old_send = sam.player.send_message
+    sam.player.old_send_message = sam.player.old_send_message or sam.player.send_message
     function sam.player.send_message(ply, msg, tbl)
         if ply == nil and debug.traceback():find("lua/sam/command/", 1, true) then
-            old_send(get_players(), msg, tbl)
+            sam.player.old_send_message(get_players(), msg, tbl)
         else
-            old_send(ply, msg, tbl)
+            sam.player.old_send_message(ply, msg, tbl)
         end
     end
 end
